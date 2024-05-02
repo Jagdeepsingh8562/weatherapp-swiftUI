01c8430c955a9bd6a9d22f29fb859bc782016f18
Jagdeep <Jagdeepsingh8562@gmail.com>
2024-05-02 15:59:44 +0530

    first commit 🚀

---
+++ weatherapp-swiftUI/weatherapp_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherapp_swiftUIApp.swift
+//  weatherapp-swiftUI
+//
+//  Created by Jagdeep Singh on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherapp_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
